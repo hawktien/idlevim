a5184821f205d3ae8cfba81d7ee77bce7747d2eb
root <wind>
2026-02-27 19:57:36 +0800

    Update nvim keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,8 +4,6 @@ pam("n", "<S-L>")
 pam("n", "<S-H>")
 
 local map = vim.keymap.set
-map("n", "<leader>ww", "<cmd>w<cr><esc>", { desc = "Save File" })
-map("n", "<leader>q", "<cmd>bd<cr><esc>", { desc = "Close Current Buffer" })
 map("n", "gl", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 map("n", "gh", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 map("n", "<C-L>", "<cmd>!/usr/bin/php -l %<cr>", { desc = "PHP Synctax Check" })
